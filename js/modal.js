d6820e512f1eb51e529b6bb988e3510dae797420
mepinit <evgenmep@gmail.com>
2023-02-27 21:17:57 +0200

    1

---
+++ js/modal.js
@@ -0,0 +1,42 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[datar-modal-open]"),
+      closeModalBtn: document.querySelector("[datar-modal-close]"),
+      modal: document.querySelector("[datar-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[datap-modal-open]"),
+      closeModalBtn: document.querySelector("[datap-modal-close]"),
+      modal: document.querySelector("[datap-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
